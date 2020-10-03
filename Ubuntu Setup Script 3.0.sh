ceaa90363db089539a8ccd605a34c5b6aef84ea1
NP Viroonc <npviro@gmail.com>
2020-10-03 11:12:43 -0700

    Add files via upload

---
+++ Ubuntu Setup Script 3.0.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# Start of Function Cluster
+mainmenu () {
+	clear
+ 	tput setaf 3
+	echo "================================="
+	echo " --- Ubuntu Setup Script 3.0 ---"
+	echo "================================="
+	echo "Supported Ubuntu Versions: 20.04 LTS"
+	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
+	tput setaf 3
+	echo "You can open this script in a text editor to see packages to be installed in detail."
+	tput setaf 9
+	echo "System will automatically reboot after the script is run!!!"
+	echo "It is not recommended to run this script more than once!!!"
+	echo "Make sure you have a stable and fast Internet connection before proceeding!!!"
+	tput setaf 3
+	echo "Press 1 to perform a Full Install (All User Packages)"
+	echo "Press 2 to perform a Minimal Install (Essentials)"
+	tput setaf 9
+	echo "Press Q to quit"
+	tput sgr0
+	echo "Enter your selection followed by <return>:"
+	tput sgr0
+	read answer
+	case "$answer" in
+		1) full;;
+		2) minimal;;
+		q) quitscript;;
+		Q) quitscript;;
+	esac
+	badoption
+}
+quitscript () {
+	tput sgr0
+	clear
+	exit
+}
+badoption () {
+	clear
+	tput setaf 9
+	echo "Invalid Option!"
+	tput setaf 3
+	echo "Returning to Main Menu..."
+	tput sgr0
+	sleep 3
+	mainmenu
+}
+finish () {
+	clear
+	tput setaf 10
+	echo "Done..."
+	tput setaf 9
+	echo "Rebooting..."
+	tput sgr0
+	sleep 3
+	clear
+	sudo reboot
+}
+full () {
+	clear
+	tput setaf 3
+	echo "Full Install/All User Packages..."
+	tput sgr0
+	sleep 3
+	clear
+	sudo apt update -y
+	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi gnome-sushi unoconv fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo add-apt-repository -y ppa:linuxuprising/java
+	sudo apt install -y oracle-java14-installer
+	java --version
+	sleep 3
+	java -version
+	sleep 3
+	sudo add-apt-repository -y ppa:mkusb/ppa
+	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
+	sudo add-apt-repository -y ppa:obsproject/obs-studio
+	sudo apt install -y obs-studio
+	sudo add-apt-repository -y ppa:minetestdevs/stable
+	sudo apt install -y minetest
+	curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt update -y
+	sudo apt install -y spotify-client
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y
+	sudo apt autoclean -y
+	sudo usermod -aG cdrom $USER
+	finish
+}
+minimal () {
+	clear
+	tput setaf 3
+	echo "Minimal Install/Essentials..."
+	tput sgr0
+	sleep 3
+	clear
+	sudo apt update -y
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gnome-tweaks gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup gnome-sushi unoconv fonts-cantarell htop curl git
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y
+	sudo apt autoclean -y
+	finish
+}
+# End of Function Cluster
+# Start of Main Script
+while true
+do
+	mainmenu
+done
+# End of Main Script
