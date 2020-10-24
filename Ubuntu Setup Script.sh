6cecbaa93976c97eb6fe3175320613e4a783177c
NP Viroonc <npviro@gmail.com>
2020-10-24 02:32:13 -0700

    Update Ubuntu Setup Script.sh

--- Ubuntu Setup Script.sh
+++ Ubuntu Setup Script.sh
@@ -4,16 +4,15 @@ mainmenu () {
 	clear
  	tput setaf 3
 	echo "================================="
-	echo " --- Ubuntu Setup Script 3.5 ---"
+	echo " --- Ubuntu Setup Script 3.6 ---"
 	echo "================================="
 	echo "Supported Ubuntu Versions: 20.04 LTS, 20.10"
 	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
-	tput setaf 3
+	echo "This script will show terminal output. This is normal."
 	echo "You can open this script in a text editor to see packages to be installed in detail."
 	tput setaf 9
 	echo "System will automatically reboot after the script is run!!!"
 	echo "It is not recommended to run this script more than once!!!"
-	echo "It is not recommended to run this script on the Ubuntu flavors!!!"
 	echo "Make sure you have a stable and fast Internet connection before proceeding!!!"
 	tput setaf 3
 	echo "Press 1 to perform a Full Install (All User Packages)"
@@ -25,12 +24,56 @@ mainmenu () {
 	tput sgr0
 	read answer
 	case "$answer" in
-		1) full;;
-		2) minimal;;
+		1) fullmenu;;
+		2) minimalmenu;;
 		q) quitscript;;
 		Q) quitscript;;
 	esac
 	badoption
+}
+fullmenu () {
+    clear
+ 	tput setaf 3
+	echo "=============================="
+	echo " --- Full Install Options ---"
+	echo "=============================="
+	echo "Press 1 to select the configuration for Ubuntu. This applies to the primary edition of Ubuntu only. Please do not use this on the official Ubuntu flavors."
+	echo "Press 2 to select the universal configuration for Ubuntu based distros. This includes official Ubuntu flavors and derivatives."
+	tput setaf 9
+	echo "Press Q to return to Main Menu"
+	tput sgr0
+	echo "Enter your selection followed by <return>:"
+	tput sgr0
+	read answer
+	case "$answer" in
+		1) full;;
+		2) fulluniversal;;
+		q) mainmenu;;
+		Q) mainmenu;;
+	esac
+	badoptionfull
+}
+minimalmenu () {
+	clear
+ 	tput setaf 3
+	echo "================================="
+	echo " --- Minimal Install Options ---"
+	echo "================================="
+	echo "Press 1 to select the configuration for Ubuntu. This applies to the primary edition of Ubuntu only. Please do not use this on the official Ubuntu flavors."
+	echo "Press 2 to select the universal configuration for Ubuntu based distros. This includes official Ubuntu flavors and derivatives."
+	tput setaf 9
+	echo "Press Q to return to Main Menu"
+	tput sgr0
+	echo "Enter your selection followed by <return>:"
+	tput sgr0
+	read answer
+	case "$answer" in
+		1) minimal;;
+		2) minimaluniversal;;
+		q) mainmenu;;
+		Q) mainmenu;;
+	esac
+	badoptionminimal
 }
 quitscript () {
 	tput sgr0
@@ -47,6 +90,26 @@ badoption () {
 	sleep 3
 	mainmenu
 }
+badoptionfull () {
+	clear
+	tput setaf 9
+	echo "Invalid Option!"
+	tput setaf 3
+	echo "Returning to Full Install Options..."
+	tput sgr0
+	sleep 3
+	fullmenu
+}
+badoptionminimal () {
+	clear
+	tput setaf 9
+	echo "Invalid Option!"
+	tput setaf 3
+	echo "Returning to Minimal Install Options..."
+	tput sgr0
+	sleep 3
+	minimalmenu
+}
 finish () {
 	clear
 	tput setaf 10
@@ -61,12 +124,12 @@ finish () {
 full () {
 	clear
 	tput setaf 3
-	echo "Full Install/All User Packages..."
+	echo "Full Install/All User Packages (Main Ubuntu)..."
 	tput sgr0
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi gnome-sushi unoconv fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gnome-shell-extensions gdebi gnome-weather gnome-firmware
+	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi gnome-sushi unoconv fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gnome-shell-extension-prefs gdebi gnome-weather gnome-firmware gucharmap menulibre minetest
 	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
 	sudo dpkg-reconfigure libdvd-pkg
 	sudo dpkg --add-architecture i386
@@ -82,8 +145,6 @@ full () {
 	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
 	sudo add-apt-repository -y ppa:obsproject/obs-studio
 	sudo apt install -y obs-studio
-	sudo add-apt-repository -y ppa:minetestdevs/stable
-	sudo apt install -y minetest
 	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 	sudo apt update -y
@@ -99,12 +160,69 @@ full () {
 minimal () {
 	clear
 	tput setaf 3
-	echo "Minimal Install/Essentials..."
+	echo "Minimal Install/Essentials (Main Ubuntu)..."
 	tput sgr0
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gnome-tweaks gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup gnome-sushi unoconv fonts-cantarell htop curl git gnome-shell-extensions gdebi gnome-firmware
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gnome-tweaks gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup gnome-sushi unoconv fonts-cantarell htop curl git gnome-shell-extension-prefs gdebi gnome-firmware gucharmap menulibre
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y
+	sudo apt autoclean -y
+	sudo snap refresh
+	finish
+}
+fulluniversal () {
+	clear
+	tput setaf 3
+	echo "Full Install/All User Packages (Universal Ubuntu)..."
+	tput sgr0
+	sleep 3
+	clear
+	sudo apt update -y
+	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gdebi gnome-weather gnome-firmware gucharmap menulibre minetest
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo add-apt-repository -y ppa:linuxuprising/java
+	sudo apt install -y oracle-java14-installer
+	java --version
+	sleep 3
+	java -version
+	sleep 3
+	sudo add-apt-repository -y ppa:mkusb/ppa
+	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
+	sudo add-apt-repository -y ppa:obsproject/obs-studio
+	sudo apt install -y obs-studio
+	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt update -y
+	sudo apt install -y spotify-client
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y
+	sudo apt autoclean -y
+	sudo snap refresh
+	sudo usermod -aG cdrom $USER
+	finish
+}
+minimaluniversal () {
+	clear
+	tput setaf 3
+	echo "Minimal Install/Essentials (Universal Ubuntu)..."
+	tput sgr0
+	sleep 3
+	clear
+	sudo apt update -y
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup fonts-cantarell htop curl git gdebi gnome-firmware gucharmap menulibre
 	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
 	sudo dpkg-reconfigure libdvd-pkg
 	sudo dpkg --add-architecture i386
